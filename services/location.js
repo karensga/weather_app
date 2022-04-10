556c21b6f5311a6a5ba0996fc01bba5050453445
Karen Sepulveda Garcia <karennads10@gmail.com>
2022-04-09 22:47:31 -0500

    change url in service

--- services/location.js
+++ services/location.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export function getLocation({ location }) {
-    const url = `https://www.metaweather.com/api/location/${location}`
+    const url = `https://whispering-stream-43221.herokuapp.com/https://www.metaweather.com/api/location/${location}`
 
-    return axios.get(url)
+    return axios.get(url)  
 
 }
--- services/searchLocation.js
+++ services/searchLocation.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default function getSearchLocation({ keyword }) {
-    const url = `https://www.metaweather.com/api/location/search/?query=${keyword}`
+    const url = `https://whispering-stream-43221.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${keyword}`
 
     return axios.get(url)
 }
